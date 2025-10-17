921086b3eae4e3157cb5674d6403fdfdcb2a6a41
Luis Alejandro Quiroga Gómez <lualquigo@gmail.com>
2025-10-17 13:07:13 -0500

    chore: fixes sqlite nix lib error

--- devenv.nix
+++ devenv.nix
@@ -1,6 +1,14 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
+    packages = [
+        pkgs.sqlite
+    ];
+
+    # As I'm using Nix, I need to use this fix:
+    # https://discourse.nixos.org/t/libsqlite3-so-no-such-file-or-directory/58556
+    env.LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath (with pkgs; [ sqlite ])}:$LD_LIBRARY_PATH";
+
     languages.rust = {
         enable = true;
     };
